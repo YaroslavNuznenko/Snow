22f99ca6b8e7b05e2721511270582a42994e03bd
YaroslavNuznenko <nuznenko@gmail.com>
2017-08-29 22:15:16 +0300

    v.1.0.1
    final editing and release

--- js/common.js
+++ js/common.js
@@ -99,3 +99,27 @@ $(document).ready(function(){
         ]
     });
 });
+/***********************************************************/
+$(document).ready(function(){
+    $(".menu").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+        top = $(id).offset().top;
+
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+        if($('.collapsed-menu').css('display')==='block'){
+            $('.collapsed-menu').css('display','none');
+            $('#scrolled_navbar').css('display','block');
+            $(document).css('overflow','hidden');
+        }
+    });
+});
+
+
+/***********************************************************/
